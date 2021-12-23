737bb84a06a888e9b6f3901b07c2782b8374d5d1
Edgar Uriel Domnguez Espinoza <edgar_uriel84@genomorro.name>
2021-12-23 06:16:56 +0000

    Sync files

--- test.py
+++ test.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.13.0
+#       jupytext_version: 1.13.4
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -16,6 +16,7 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
+# %matplotlib inline
 
 # %%
 print(cv.__version__)
@@ -60,3 +61,49 @@ plt.imshow(grayscale)
 plt.title("Imagen en escala de grises con promedios")
 plt.show()
 cv.imwrite('out/gs-p.png', grayscale)
+
+
+# %% [markdown]
+# Filtro de mediana
+# https://github.com/MeteHanC/Python-Median-Filter/blob/master/MedianFilter.py
+
+# %%
+def median_filter(data, filter_size):
+    temp = []
+    indexer = filter_size // 2
+    window = [
+        (i, j)
+        for i in range(-indexer, filter_size-indexer)
+        for j in range(-indexer, filter_size-indexer)
+    ]
+    index = len(window) // 2
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            data[i][j] = sorted(
+                0 if (
+                    min(i+a, j+b) < 0
+                    or len(data) <= i+a
+                    or len(data[0]) <= j+b
+                ) else data[i+a][j+b]
+                for a, b in window
+            )[index]
+    return data
+
+# %%
+from PIL import Image
+
+im_noise = Image.open("im/noisyimg.png").convert("L")
+plt.imshow(im_noise)
+plt.title("Imagen original")
+plt.show()
+
+# %%
+arr = np.array(im_noise)
+removed_noise = median_filter(arr, 5) 
+im_filtered = Image.fromarray(removed_noise)
+plt.imshow(im_filtered)
+plt.title("Imagen filtrada")
+plt.show()
+im_filtered = im_filtered.save("out/filtered.jpg")
+
+# %%
